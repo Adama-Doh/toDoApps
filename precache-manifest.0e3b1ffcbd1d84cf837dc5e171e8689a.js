6382065b1da46cd55eba6aa406b8c6047c1a8ff1
Samuel Adama-Doh <samueladamadoh@gmail.com>
2019-12-23 14:02:29 +0000

    Initial Commit

---
+++ precache-manifest.0e3b1ffcbd1d84cf837dc5e171e8689a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "160784cc19edc51546521f63b7443ebd",
+    "url": "/toDoApp/index.html"
+  },
+  {
+    "revision": "3d426eea556a811581ae",
+    "url": "/toDoApp/static/css/main.a6c49b6d.chunk.css"
+  },
+  {
+    "revision": "3da17989d5c5da24ec10",
+    "url": "/toDoApp/static/js/2.47b8276d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/toDoApp/static/js/2.47b8276d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3d426eea556a811581ae",
+    "url": "/toDoApp/static/js/main.a2912067.chunk.js"
+  },
+  {
+    "revision": "806fb451b1a784ad254a",
+    "url": "/toDoApp/static/js/runtime-main.5e567790.js"
+  }
+]);
